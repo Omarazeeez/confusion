73594d6907c1b0b4efb8a74244f720641dc482e3
Umer <umer360@hotmail.com>
2020-01-02 02:02:58 +0500

    React Component_P1

---
+++ src/Components/menuComponent.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import {Media} from 'reactstrap';
+
+
+class Menu extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dishes: [                                   //dishes prop consists of 4 js objects with their objects.
+                {
+                    id: 0,
+                    name:'Uthappizza',
+                    image: 'assets/images/uthappizza.png',
+                    category: 'mains',
+                    label:'Hot',
+                    price:'4.99',
+                    description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        
+                },
+                
+                {
+                    id: 1,
+                    name:'Zucchipakoda',
+                    image: 'assets/images/zucchipakoda.png',
+                    category: 'appetizer',
+                    label:'',
+                    price:'1.99',
+                    description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        
+                },
+
+                {
+                    id: 2,
+                    name:'Vadonut',
+                    image: 'assets/images/vadonut.png',
+                    category: 'appetizer',
+                    label:'New',
+                    price:'1.99',
+                    description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        
+                },
+
+                {
+                    id: 3,
+                    name:'ElaiCheese Cake',
+                    image: 'assets/images/elaicheesecake.png',
+                    category: 'dessert',
+                    label:'',
+                    price:'2.99',
+                    description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        
+                }
+            
+            ]
+
+        }
+    }
+
+    render() {
+        //Renders list of dishes through Menu component
+
+        const menu= this.state.dishes.map((dish) =>{         //maps each dish onto screen
+            return(                                          //col12 mt  means width of whole page(col12) ans margin of 5
+                <div key={dish.id} className="col-12 mt-5">         
+                    <Media tag="li">
+                        
+                        <Media left middle>
+
+                            <Media object src={dish.image} alt={dish.name}/> 
+                        
+                        </Media>
+
+                        <Media body className="ml-5">
+
+                            <Media heading>{dish.name}</Media>
+                            <p>{dish.description}</p>
+
+                        </Media>
+
+                    </Media>
+                </div>
+            );      //inside div i constructed view of each of the items in my dishes
+        });
+        return (
+            //menu variable used in return scope 
+
+            <div className="container">
+                <div className="row">
+                   <Media list>
+                        {menu}      
+                    </Media> 
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
