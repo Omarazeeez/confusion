a3778b0e83b1d680846b4b86372f2c4cf45f69b0
Umer <umer360@hotmail.com>
2020-02-09 19:43:05 +0500

    React Router

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Menu from './menuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {DISHES} from '../shared/dishes';
 import DishDetail from './DishDetailComponent';
 
@@ -13,21 +15,25 @@ class Main extends Component{
 
     this.state= {
       dishes:DISHES, //dishes.js file
-      selectedDish: null
     };
   }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId })
-    }
   render() {
+
+    const HomePage= () => {
+      return(
+        <Home />
+      );
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes}
-            onClick={(dishId) => this.onDishSelect(dishId)}/>  
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component ={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+        </Switch>
         <div>
-          {this.state.selectedDish && <DishDetail dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish )[0]} />}
           <Footer />
         </div>
       </div>
@@ -36,5 +42,7 @@ class Main extends Component{
   }
   
 }
+
+
 
 export default Main;
